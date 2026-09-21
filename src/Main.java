026542a6889763fa87074121617d9cd07d5fa68a
soradacroi <soradacroi@gmail.com>
2026-09-21 23:14:12 +0530

    meow

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Date;
+
 public class Main {
   public static void main(String[] args) {
     // code here
+    byte age = 20;
+    age = 26;
+    int myAge = age;
     System.out.println("Hello World");
+    System.out.println(age);
+    System.out.print(myAge);
+    System.out.print("hello\n");
+    long numberOne = 2_345_455_456L;
+    float price = 19.99F;
+    char firstLetter = 'A';
+    String fullName = "Ashura";
+    boolean isEligible = true;
+    Date now = new Date();
+    System.out.println(now.getTime());
+    System.out.println(now);
   }
 }
